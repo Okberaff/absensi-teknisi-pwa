8a530bb11684668272f903a15dea4336937a0df7
Okberaff <afriansyahokber@gmail.com>
2026-05-18 04:57:43 +0700

    edit kendala

--- src/App.tsx
+++ src/App.tsx
@@ -2170,8 +2170,7 @@ export default function App() {
                         onChange={(e) =>
                           updateKeteranganKendala(order.noWo, e.target.value)
                         }
-                        placeholder=
-                      />
+                        placeholder/>
 
                       <label>Upload Foto Kendala</label>
                       <input
